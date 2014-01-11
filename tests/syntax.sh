eff61cb57df3f2ced1dd0d68d0920f7219cfd689
Zach Borboa <zachborboa@gmail.com>
2014-01-10 23:45:17 -0800

    Add coding standard test script

---
+++ tests/syntax.sh
@@ -0,0 +1,8 @@
+# FIXME: Use ruleset.xml
+phpcs ../lib/Curl.class.php | \
+    grep -v "| ERROR   | Expected \"} else {\\\n\"; found \"}\\\n" | \
+    grep -v "| ERROR   | Missing class doc comment"                | \
+    grep -v "| ERROR   | Missing file doc comment"                 | \
+    grep -v "| ERROR   | Missing function doc comment"             | \
+    grep -v "| ERROR   | Opening brace should be on a new line"    | \
+    grep -v "| WARNING | Line exceeds 85 characters;"
