8edfb697f41cac5242044120a8cf65860e007f5c
Zach Borboa <zachborboa@gmail.com>
2014-03-03 20:49:32 -0800

    Update test url path

--- tests/PHPCurlClass/helper.inc.php
+++ tests/PHPCurlClass/helper.inc.php
@@ -1,6 +1,6 @@
 <?php
 class Test {
-    const TEST_URL = 'https://127.0.0.1/php-curl-class/tests/server.php';
+    const TEST_URL = 'https://127.0.0.1/php-curl-class/tests/PHPCurlClass/server.php';
     const ERROR_URL = 'https://1.2.3.4/';
 
     function __construct() {
