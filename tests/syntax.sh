967181499a84d8a7da77fbd37c56bd4bb14d0675
Zach Borboa <zachborboa@gmail.com>
2014-03-29 01:19:49 -0700

    Update syntax test script

--- tests/syntax.sh
+++ tests/syntax.sh
@@ -1,8 +1,1 @@
-# FIXME: Use ruleset.xml
-phpcs ../src/Curl.class.php | \
-    grep -v "| ERROR   | Expected \"} else {\\\n\"; found \"}\\\n" | \
-    grep -v "| ERROR   | Missing class doc comment"                | \
-    grep -v "| ERROR   | Missing file doc comment"                 | \
-    grep -v "| ERROR   | Missing function doc comment"             | \
-    grep -v "| ERROR   | Opening brace should be on a new line"    | \
-    grep -v "| WARNING | Line exceeds 85 characters;"
+phpcs --standard=PSR2 ../src/Curl.class.php
