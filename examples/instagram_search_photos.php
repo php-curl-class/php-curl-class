5e7b16b7b0c6a1fd1c40f27cd715f28104612cab
Zach Borboa <zachborboa@gmail.com>
2014-03-30 07:31:05 -0700

    Add Instagram search photos example

---
+++ examples/instagram_search_photos.php
@@ -0,0 +1,17 @@
+<?php
+require '../src/Curl.class.php';
+
+
+define('INSTAGRAM_CLIENT_ID', 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+
+$curl = new Curl();
+$curl->get('https://api.instagram.com/v1/media/search', array(
+    'client_id' => INSTAGRAM_CLIENT_ID,
+    'lat' => '37.8296',
+    'lng' => '-122.4832',
+));
+
+foreach ($curl->response->data as $media) {
+    $image = $media->images->low_resolution;
+    echo '<img alt="" src="' . $image->url . '" width="' . $image->width . '" height="' . $image->height . '" />';
+}
