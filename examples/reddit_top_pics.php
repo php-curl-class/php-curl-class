aed7d3e9997d83505a36331831f07ac494c57263
Zach Borboa <zachborboa@gmail.com>
2014-05-20 22:08:46 -0700

    Add Reddit pics example

---
+++ examples/reddit_top_pics.php
@@ -0,0 +1,28 @@
+<?php
+require '../src/Curl.class.php';
+
+
+$data = array();
+if (isset($_GET['after'])) {
+    $data['after'] = $_GET['after'];
+}
+
+$curl = new Curl();
+$curl->get('http://www.reddit.com/r/pics/top/.json', $data);
+
+echo '<ul>';
+
+foreach ($curl->response->data->children as $result) {
+    $pic = $result->data;
+    echo
+        '<li>' .
+            '<a href="' . $pic->url . '" target="_blank">' .
+                $pic->title . '<br />' .
+                '<img alt="" src="' . $pic->thumbnail . '" />' .
+            '</a> ' .
+            $pic->score . ' pts ' . $pic->num_comments . ' comments by ' . $pic->author .
+        '</li>';
+}
+
+echo '</ul>';
+echo '<a href="?after=' . $curl->response->data->after . '">Next</a>';
