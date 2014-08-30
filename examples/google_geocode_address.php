9fb14c98873b012e1f5b82a4f5d5cbc0a5643857
Zach Borboa <zachborboa@gmail.com>
2014-08-29 19:54:55 -0700

    Add Google Geocoding example

---
+++ examples/google_geocode_address.php
@@ -0,0 +1,19 @@
+<?php
+require '../src/Curl/Curl.php';
+
+use \Curl\Curl;
+
+
+$address = 'Paris, France';
+$curl = new Curl();
+$curl->get('http://maps.googleapis.com/maps/api/geocode/json', array(
+    'address' => $address,
+));
+
+if ($curl->response->status === 'OK') {
+    $result = $curl->response->results['0'];
+    echo
+        $result->formatted_address . ' is located at ' .
+        'latitude ' . $result->geometry->location->lat . ' and ' .
+        'longitude ' .  $result->geometry->location->lng . '.';
+}
