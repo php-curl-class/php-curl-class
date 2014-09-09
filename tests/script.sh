351f4b51abc3d8d051c1ead2e93659ae9cab1b92
zborboa-google <zborboa@google.com>
2014-09-09 11:34:27 -0700

    Add script comments

--- tests/script.sh
+++ tests/script.sh
@@ -1,9 +1,12 @@
+# Check syntax in php files.
 find . -type "f" -iname "*.php" | xargs -L "1" php -l
 
+# Check for trailing whitespace in php files.
 trailing_whitespace=$(find . -type "f" -iname "*.php" -exec egrep --line-number -H " +$" {} \;)
 if [[ ! -z "${trailing_whitespace}" ]]; then
     echo -e "${trailing_whitespace}" | perl -pe 's/^(.*)$/Trailing whitespace found in \1/'
     exit 1
 fi
 
+# Run tests.
 cd tests && phpunit --configuration phpunit.xml
