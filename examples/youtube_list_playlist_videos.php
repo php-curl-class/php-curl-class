ba938c24ed3dd66d6f27eb467eb89d26623da09b
Zach Borboa <zachborboa@gmail.com>
2014-10-27 18:14:07 -0700

    Add YouTube example to list videos in a playlist

---
+++ examples/youtube_list_playlist_videos.php
@@ -0,0 +1,26 @@
+<?php
+require '../src/Curl/Curl.php';
+
+use \Curl\Curl;
+
+
+define('YOUTUBE_API_KEY', 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+
+$playlistId = 'RDHJb0VYVtaNc';
+
+$curl = new Curl();
+$curl->get('https://www.googleapis.com/youtube/v3/playlistItems', array(
+    'key' => YOUTUBE_API_KEY,
+    'maxResults' => '50',
+    'part' => 'snippet',
+    'playlistId' => $playlistId,
+));
+
+echo 'Songs in this playlist:' . "\n";
+
+foreach ($curl->response->items as $item) {
+    echo
+        $item->snippet->title . "\n" .
+        'https://www.youtube.com/watch?v=' . $item->snippet->resourceId->videoId . "\n" .
+        "\n";
+}
