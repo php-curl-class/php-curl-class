cf3935b3c0d316642198ee5cef260cab1f8cc198
Michael Mulligan <michael@bigroomstudios.com>
2014-12-18 12:46:58 -0500

    Replacement `http_parse_headers` function for systems that do not have it.

--- src/Curl/Curl.php
+++ src/Curl/Curl.php
@@ -605,3 +605,41 @@ class CaseInsensitiveArray implements \ArrayAccess, \Countable, \Iterator
         reset($this->container);
     }
 }
+
+if (!function_exists('http_parse_headers')) {
+    function http_parse_headers($raw_headers)
+    {
+        $headers = array();
+        $key = '';
+
+        foreach(explode("\n", $raw_headers) as $i => $h)
+        {
+            $h = explode(':', $h, 2);
+
+            if (isset($h[1]))
+            {
+                if (!isset($headers[$h[0]]))
+                    $headers[$h[0]] = trim($h[1]);
+                elseif (is_array($headers[$h[0]]))
+                {
+                    $headers[$h[0]] = array_merge($headers[$h[0]], array(trim($h[1]))); // [+]
+                }
+                else
+                {
+                    $headers[$h[0]] = array_merge(array($headers[$h[0]]), array(trim($h[1]))); // [+]
+                }
+
+                $key = $h[0];
+            }
+            else
+            {
+                if (substr($h[0], 0, 1) == "\t")
+                    $headers[$key] .= "\r\n\t".trim($h[0]);
+                elseif (!$key)
+                    $headers[0] = trim($h[0]);trim($h[0]);
+            }
+        }
+
+        return $headers;
+    }
+}
