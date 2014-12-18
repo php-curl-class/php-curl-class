dd0ad0bedc590ebc7f60de7472c8b05953594e92
Michael Mulligan <michael@bigroomstudios.com>
2014-12-18 14:21:42 -0500

    Don't mess with function declarations.

--- src/Curl/Curl.php
+++ src/Curl/Curl.php
@@ -378,9 +378,47 @@ class Curl
         $pecl = !empty($this->pecl_headers);
         list($first, $raw_headers) = preg_split('/\r\n/', $raw_headers, 2, PREG_SPLIT_NO_EMPTY);
         if($pecl) {
-            foreach(http_parse_headers($raw_headers) as $key => $value) {
+			
+			if (function_exists('http_parse_headers')) {
+				$headers = http_parse_headers($raw_headers);
+			} else {
+				$headers = array();
+				$key = '';
+			
+				foreach(explode("\n", $raw_headers) as $i => $h)
+				{
+					$h = explode(':', $h, 2);
+			
+					if (isset($h[1]))
+					{
+						if (!isset($headers[$h[0]]))
+							$headers[$h[0]] = trim($h[1]);
+						elseif (is_array($headers[$h[0]]))
+						{
+							$headers[$h[0]] = array_merge($headers[$h[0]], array(trim($h[1]))); // [+]
+						}
+						else
+						{
+							$headers[$h[0]] = array_merge(array($headers[$h[0]]), array(trim($h[1]))); // [+]
+						}
+			
+						$key = $h[0];
+					}
+					else
+					{
+						if (substr($h[0], 0, 1) == "\t")
+							$headers[$key] .= "\r\n\t".trim($h[0]);
+						elseif (!$key)
+							$headers[0] = trim($h[0]);trim($h[0]);
+					}
+				}
+			
+			}
+			
+            foreach(http_parse_headers($headers) as $key => $value) {
                 $http_headers[$key] = $value;
             }
+			
         } else {
             $raw_headers = preg_split('/\r\n/', $raw_headers, null, PREG_SPLIT_NO_EMPTY);
             
@@ -661,43 +699,5 @@ class CaseInsensitiveArray implements \ArrayAccess, \Countable, \Iterator
     public function rewind()
     {
         reset($this->container);
-    }
-}
-
-if (!function_exists('http_parse_headers')) {
-    function http_parse_headers($raw_headers)
-    {
-        $headers = array();
-        $key = '';
-
-        foreach(explode("\n", $raw_headers) as $i => $h)
-        {
-            $h = explode(':', $h, 2);
-
-            if (isset($h[1]))
-            {
-                if (!isset($headers[$h[0]]))
-                    $headers[$h[0]] = trim($h[1]);
-                elseif (is_array($headers[$h[0]]))
-                {
-                    $headers[$h[0]] = array_merge($headers[$h[0]], array(trim($h[1]))); // [+]
-                }
-                else
-                {
-                    $headers[$h[0]] = array_merge(array($headers[$h[0]]), array(trim($h[1]))); // [+]
-                }
-
-                $key = $h[0];
-            }
-            else
-            {
-                if (substr($h[0], 0, 1) == "\t")
-                    $headers[$key] .= "\r\n\t".trim($h[0]);
-                elseif (!$key)
-                    $headers[0] = trim($h[0]);trim($h[0]);
-            }
-        }
-
-        return $headers;
     }
 }
