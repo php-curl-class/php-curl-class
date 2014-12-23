1a0e9c1f4b2fa9cda083825d0724fc36f724b7d9
Chad Whitacre <chad@zetaweb.com>
2014-12-23 14:52:49 -0500

    catch up with rename
    Filename and envvars were updated, but platform and URL weren't.

--- examples/gratipay_send_tip.php
+++ examples/gratipay_send_tip.php
@@ -9,12 +9,12 @@ define('GRATIPAY_API_KEY', 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX');
 $data = array(
     array(
         'username' => 'user' . mt_rand(),
-        'platform' => 'gittip',
+        'platform' => 'gratipay',
         'amount' =>  '0.02',
     ),
     array(
         'username' => 'user' . mt_rand(),
-        'platform' => 'gittip',
+        'platform' => 'gratipay',
         'amount' =>  '0.02',
     ),
 );
@@ -22,7 +22,7 @@ $data = array(
 $curl = new Curl();
 $curl->setHeader('Content-Type', 'application/json');
 $curl->setBasicAuthentication(GRATIPAY_API_KEY);
-$curl->post('https://www.gittip.com/' . GRATIPAY_USERNAME . '/tips.json', json_encode($data));
+$curl->post('https://gratipay.com/' . GRATIPAY_USERNAME . '/tips.json', json_encode($data));
 
 foreach ($curl->response as $tip) {
     echo $tip->amount . ' given to ' . $tip->username . '.' . "\n";
