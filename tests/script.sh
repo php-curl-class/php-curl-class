c1cf3b3f08470a5666dc3ff17cb65cf17359b802
Zach Borboa <zachborboa@gmail.com>
2015-01-01 16:19:12 -0800

    Enforce line ending consistency in php files

--- tests/script.sh
+++ tests/script.sh
@@ -4,7 +4,14 @@ find . -type "f" -iname "*.php" | xargs -L "1" php -l
 # Run tests.
 cd tests && phpunit --configuration phpunit.xml
 
-# Prohibit tab characters in php files.
+# Enforce line ending consistency in php files.
+crlf_file=$(find . -type "f" -iname "*.php" -exec grep --files-with-matches $'\r' {} \;)
+if [[ ! -z "${crlf_file}" ]]; then
+    echo "${crlf_file}" | perl -pe 's/(.*)/CRLF line terminators found in \1/'
+    exit 1
+fi
+
+# Enforce indentation character consistency in php files.
 tab_char=$(find . -type "f" -iname "*.php" -exec grep --line-number -H --perl-regexp "\t" {} \;)
 if [[ ! -z "${tab_char}" ]]; then
     echo -e "${tab_char}" | perl -pe 's/^(.*)$/Tab character found in \1/'
