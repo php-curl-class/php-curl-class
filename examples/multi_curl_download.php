63912a32bed1f28bbb781c4a3cf96a0a99d3dabd
Zach Borboa <zachborboa@gmail.com>
2015-03-31 08:14:19 -0700

    Update MultiCurl::addDownload() example

--- examples/multi_curl_download.php
+++ examples/multi_curl_download.php
@@ -6,6 +6,6 @@ use \Curl\Curl;
 use \Curl\MultiCurl;
 
 $multi_curl = new MultiCurl();
-$multi_curl->download('https://php.net/images/logos/php-med-trans.png', '/tmp/php-med-trans.png');
-$multi_curl->download('https://upload.wikimedia.org/wikipedia/commons/c/c1/PHP_Logo.png', '/tmp/PHP_Logo.png');
+$multi_curl->addDownload('https://php.net/images/logos/php-med-trans.png', '/tmp/php-med-trans.png');
+$multi_curl->addDownload('https://upload.wikimedia.org/wikipedia/commons/c/c1/PHP_Logo.png', '/tmp/PHP_Logo.png');
 $multi_curl->start();
