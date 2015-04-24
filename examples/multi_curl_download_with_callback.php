6c2b84862d60ee0f0e9b2965c766a5540a23b536
Zach Borboa <zachborboa@gmail.com>
2015-04-24 03:38:46 -0700

    Add download file with callback examples

---
+++ examples/multi_curl_download_with_callback.php
@@ -0,0 +1,18 @@
+<?php
+require '../src/Curl/Curl.php';
+require '../src/Curl/MultiCurl.php';
+
+use \Curl\Curl;
+use \Curl\MultiCurl;
+
+$callback = function($instance, $tmpfile) {
+    $save_to_path = '/tmp/' . basename($instance->url);
+    $fh = fopen($save_to_path, 'wb');
+    stream_copy_to_stream($tmpfile, $fh);
+    fclose($fh);
+};
+
+$multi_curl = new MultiCurl();
+$multi_curl->addDownload('https://php.net/images/logos/php-med-trans.png', $callback);
+$multi_curl->addDownload('https://upload.wikimedia.org/wikipedia/commons/c/c1/PHP_Logo.png', $callback);
+$multi_curl->start();
