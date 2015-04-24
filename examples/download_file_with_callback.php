c6ea53b95311c5b982689bdf96ef87f0703c20a0
Zach Borboa <zachborboa@gmail.com>
2015-04-24 12:45:04 -0700

    Add setURL examples

--- examples/download_file_with_callback.php
+++ examples/download_file_with_callback.php
@@ -5,7 +5,7 @@ use \Curl\Curl;
 
 $curl = new Curl();
 $curl->download('https://php.net/images/logos/php-med-trans.png', function($instance, $tmpfile) {
-    $save_to_path = '/tmp/_' . basename($instance->url);
+    $save_to_path = '/tmp/' . basename($instance->url);
     $fh = fopen($save_to_path, 'wb');
     stream_copy_to_stream($tmpfile, $fh);
     fclose($fh);
---
+++ examples/set_url_1.php
@@ -0,0 +1,18 @@
+<?php
+require '../src/Curl/Curl.php';
+
+use \Curl\Curl;
+
+// Retrieve first N pages of search results.
+$pages = 10;
+$q = 'coffee';
+
+$curl = new Curl('https://www.example.com/search');
+
+for ($i = 1; $i <= $pages; $i++) {
+    // https://www.example.com/search?q=coffee&page=N
+    $curl->get(array(
+        'q' => $q,
+        'page' => $i,
+    ));
+}
