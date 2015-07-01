2e56b4b435583f5b5094826e232f4544b8cb0fb6
Zach Borboa <zachborboa@gmail.com>
2015-07-01 00:39:13 -0700

    Add homepage placeholder

---
+++ docs/source/index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta http-equiv="refresh" content="1; url=https://github.com/php-curl-class/php-curl-class">
+<title>PHP Curl Class</title>
+</head>
+<body>
+</body>
+</html>
