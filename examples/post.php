2fd09c6061b96f24b53c52c38f343a6447ad6b0d
Zach Borboa <zachborboa@gmail.com>
2015-07-07 10:51:47 -0700

    Add POST examples

---
+++ examples/post.php
@@ -0,0 +1,19 @@
+<?php
+require '../src/Curl/Curl.php';
+
+use \Curl\Curl;
+
+// curl \
+//     -X POST \
+//     -d "id=1&content=Hello+world%21&date=2015-06-30+19%3A42%3A21" \
+//     "https://httpbin.org/post"
+
+$data = array(
+    'id' => '1',
+    'content' => 'Hello world!',
+    'date' => date('Y-m-d H:i:s'),
+);
+
+$curl = new Curl();
+$curl->post('https://httpbin.org/post', $data);
+var_dump($curl->response->form);
---
+++ examples/post_json.php
@@ -0,0 +1,20 @@
+<?php
+require '../src/Curl/Curl.php';
+
+use \Curl\Curl;
+
+// curl \
+//     -X POST \
+//     -d "{"id":"1","content":"Hello world!","date":"2015-06-30 19:42:21"}" \
+//     "https://httpbin.org/post"
+
+$data = json_encode(array(
+    'id' => '1',
+    'content' => 'Hello world!',
+    'date' => date('Y-m-d H:i:s'),
+));
+
+$curl = new Curl();
+$curl->setHeader('Content-Type', 'application/json');
+$curl->post('https://httpbin.org/post', $data);
+var_dump($curl->response->json);
