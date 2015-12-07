3518e5eb7ce53967a78c1b2c34b6ae5320f96086
0xff00ff <0xff00ff@gmail.com>
2015-12-07 14:58:37 +0200

    added php doc. phpstorm shows error on this property.

--- src/Curl/Curl.php
+++ src/Curl/Curl.php
@@ -83,6 +83,9 @@ class Curl
     private $headers = array();
     private $options = array();
 
+    /**
+     * @var callable
+     */
     private $jsonDecoder = null;
     private $jsonPattern = '/^(?:application|text)\/(?:[a-z]+(?:[\.-][0-9a-z]+){0,}[\+\.]|x-)?json(?:-[a-z]+)?/i';
     private $xmlPattern = '~^(?:text/|application/(?:atom\+|rss\+)?)xml~i';
