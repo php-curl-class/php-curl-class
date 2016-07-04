9d94a9b8a10a8af1cc0798ffc2c350532425e158
Zach Borboa <zachborboa@gmail.com>
2016-07-04 02:46:44 -0700

    Add HTTP GET examples with and without using base url

---
+++ examples/get_base_url_1.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use \Curl\Curl;
+
+$curl = new Curl('https://httpbin.org/get');
+for ($i = 1; $i <= 10; $i++) {
+    $curl->get(array(
+        'page' => $i,
+    ));
+    // TODO: Do something with result $curl->response.
+}
---
+++ examples/get_base_url_2.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use \Curl\Curl;
+
+$curl = new Curl();
+$curl->setUrl('https://httpbin.org/get');
+for ($i = 1; $i <= 10; $i++) {
+    $curl->get(array(
+        'page' => $i,
+    ));
+    // TODO: Do something with result $curl->response.
+}
