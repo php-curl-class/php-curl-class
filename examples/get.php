cd19c3bd2ddc63da3650d944b27d03f0dafc1c71
Zach Borboa <zachborboa@gmail.com>
2016-08-07 15:29:21 -0700

    Simplify get example

--- examples/get.php
+++ examples/get.php
@@ -4,9 +4,12 @@ require __DIR__ . '/vendor/autoload.php';
 use \Curl\Curl;
 
 $curl = new Curl();
-for ($i = 1; $i <= 10; $i++) {
-    $curl->get('https://httpbin.org/get', array(
-        'page' => $i,
-    ));
-    // TODO: Do something with result $curl->response.
+$curl->get('https://httpbin.org/get');
+
+if ($curl->error) {
+    echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage;
+}
+else {
+    echo 'Response:' . "\n";
+    var_dump($curl->response);
 }
