c86e86d9171f237491c72dbca194149d1d2f3243
Zach Borboa <zachborboa@gmail.com>
2017-03-01 19:00:47 -0800

    Fix #428: Add custom request example

---
+++ examples/custom.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use \Curl\Curl;
+
+
+$curl = new Curl();
+$curl->setOpt(CURLOPT_CUSTOMREQUEST, 'GET');
+$curl->setOpt(CURLOPT_NOBODY, true);
+$curl->setOpt(CURLOPT_HEADER, true);
+$curl->setUrl('https://httpbin.org/get');
+$curl->exec();
+
+if ($curl->error) {
+    echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . "\n";
+} else {
+    echo 'Response:' . "\n";
+    var_dump($curl->response);
+}
