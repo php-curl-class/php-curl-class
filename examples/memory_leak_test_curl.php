a97721c8eade81905761a73341bf4150a7b0033e
Zach Borboa <zachborboa@gmail.com>
2017-04-30 13:19:59 -0700

    Add memory leak test examples

---
+++ examples/memory_leak_test_curl.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use \Curl\Curl;
+
+$curl = new Curl();
+
+for ($i = 0; $i < 10; $i++) {
+    for ($j = 0; $j <= 500; $j++) {
+        $curl->get('http://127.0.0.1:8000/');
+    }
+    echo 'memory ' . $i . ': ' . memory_get_usage(true) . "\n";
+}
---
+++ examples/memory_leak_test_multi_curl.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use \Curl\MultiCurl;
+
+$multi_curl = new MultiCurl();
+
+for ($i = 0; $i < 10; $i++) {
+    for ($j = 0; $j <= 500; $j++) {
+        $multi_curl->addGet('http://127.0.0.1:8000/');
+    }
+    $multi_curl->start();
+    echo 'memory ' . $i . ': ' . memory_get_usage(true) . "\n";
+}
