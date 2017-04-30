b754ebf065627538136ad9c19c5d36b3212829e2
Zach Borboa <zachborboa@gmail.com>
2017-04-30 13:29:12 -0700

    Add Coinbase ETH spot price example

--- examples/coinbase_btc_spot_price.php
+++ examples/coinbase_btc_spot_price.php
@@ -5,8 +5,8 @@ use \Curl\Curl;
 
 $curl = new Curl();
 $curl->setHeader('CB-VERSION', '2016-01-01');
-$curl->get('https://api.coinbase.com/v2/prices/spot');
+$curl->get('https://api.coinbase.com/v2/prices/BTC-USD/spot');
 
 echo
-    'The current price of bitcoin at Coinbase is ' .
+    'The current price of BTC at Coinbase is ' .
     '$' . $curl->response->data->amount . ' ' . $curl->response->data->currency . '.' . "\n";
---
+++ examples/coinbase_eth_spot_price.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use \Curl\Curl;
+
+$curl = new Curl();
+$curl->setHeader('CB-VERSION', '2016-01-01');
+$curl->get('https://api.coinbase.com/v2/prices/ETH-USD/spot');
+
+echo
+    'The current price of ETH at Coinbase is ' .
+    '$' . $curl->response->data->amount . ' ' . $curl->response->data->currency . '.' . "\n";
