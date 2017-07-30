a4cf524119537b1ab59a4b3489fa4e5555ace666
Zach Borboa <zachborboa@gmail.com>
2017-07-30 14:27:20 -0700

    Fix #459: Add example with json response as associative array

---
+++ examples/post_json_array_response.php
@@ -0,0 +1,21 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use \Curl\Curl;
+
+// curl \
+//     -X POST \
+//     -d "{"id":"1","content":"Hello world!","date":"2015-06-30 19:42:21"}" \
+//     "https://httpbin.org/post"
+
+$data = array(
+    'id' => '1',
+    'content' => 'Hello world!',
+    'date' => date('Y-m-d H:i:s'),
+);
+
+$curl = new Curl();
+$curl->setDefaultJsonDecoder($assoc=true);
+$curl->setHeader('Content-Type', 'application/json');
+$curl->post('https://httpbin.org/post', $data);
+var_dump($curl->response['json']);
