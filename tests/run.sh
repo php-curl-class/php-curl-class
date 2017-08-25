96623ba839b82215e7c1a13c2c88b5a389aeb9fe
Zach Borboa <zachborboa@gmail.com>
2017-08-25 00:16:43 -0700

    Allow the test script to be run from outside the tests directory

--- tests/run.sh
+++ tests/run.sh
@@ -1,9 +1,13 @@
 set -x
+
+SCRIPT_PATH="$(cd "$(dirname "$0")"; pwd -P)"
+cd "${SCRIPT_PATH}"
+
 php -S 127.0.0.1:8000 -t PHPCurlClass/ &> /dev/null &
 pid="${!}"
 extra_args="${@}"
 phpunit \
-    --configuration phpunit.xml \
+    --configuration "phpunit.xml" \
     --debug \
     --verbose \
     ${extra_args}
