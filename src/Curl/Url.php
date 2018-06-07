703d0acfa0097a7bcbd30f23047f0e6cae56dddc
Zach Borboa <zachborboa@gmail.com>
2018-06-07 03:18:01 -0700

    Clean up

--- src/Curl/Url.php
+++ src/Curl/Url.php
@@ -193,15 +193,15 @@ class Url
      * Combine url components into a url.
      */
     private function unparseUrl($parsed_url) {
-        $scheme   = isset($parsed_url['scheme'])   ? $parsed_url['scheme'] . '://' : '';
-        $user     = isset($parsed_url['user'])     ? $parsed_url['user']           : '';
-        $pass     = isset($parsed_url['pass'])     ? ':' . $parsed_url['pass']     : '';
-        $pass     = ($user || $pass)               ? $pass . '@'                   : '';
-        $host     = isset($parsed_url['host'])     ? $parsed_url['host']           : '';
-        $port     = isset($parsed_url['port'])     ? ':' . $parsed_url['port']     : '';
-        $path     = isset($parsed_url['path'])     ? $parsed_url['path']           : '';
-        $query    = isset($parsed_url['query'])    ? '?' . $parsed_url['query']    : '';
-        $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment'] : '';
+        $scheme   = isset($parsed_url['scheme'])   ?       $parsed_url['scheme'] . '://' : '';
+        $user     = isset($parsed_url['user'])     ?       $parsed_url['user']           : '';
+        $pass     = isset($parsed_url['pass'])     ? ':' . $parsed_url['pass']           : '';
+        $pass     = ($user || $pass)               ?       $pass . '@'                   : '';
+        $host     = isset($parsed_url['host'])     ?       $parsed_url['host']           : '';
+        $port     = isset($parsed_url['port'])     ? ':' . $parsed_url['port']           : '';
+        $path     = isset($parsed_url['path'])     ?       $parsed_url['path']           : '';
+        $query    = isset($parsed_url['query'])    ? '?' . $parsed_url['query']          : '';
+        $fragment = isset($parsed_url['fragment']) ? '#' . $parsed_url['fragment']       : '';
         $unparsed_url =  $scheme . $user . $pass . $host . $port . $path . $query . $fragment;
         return $unparsed_url;
     }
