eca70c8d41ad50b16c0d17fa8a794085778b609d
Zach Borboa <zachborboa@gmail.com>
2018-06-19 01:23:24 -0700

    Add custom xml decoder example

---
+++ examples/use_custom_xml_decoder.php
@@ -0,0 +1,23 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use \Curl\Curl;
+
+$my_xml_decoder = function ($response) {
+    $xml_obj = @simplexml_load_string($response);
+    if ($xml_obj !== false) {
+        $response = json_decode(json_encode($xml_obj), true);
+    }
+    return $response;
+};
+
+$curl = new Curl();
+$curl->setXmlDecoder($my_xml_decoder);
+$curl->get('https://httpbin.org/xml');
+
+if ($curl->error) {
+    echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . "\n";
+} else {
+    echo 'Response:' . "\n";
+    var_dump($curl->response);
+}
