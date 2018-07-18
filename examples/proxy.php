29dd124a6d5eee512128722362bc58fcc5f4a851
Zach Borboa <zachborboa@gmail.com>
2018-07-17 20:16:55 -0700

    Use Curl::setProxy() in proxy example

--- examples/proxy.php
+++ examples/proxy.php
@@ -4,8 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use \Curl\Curl;
 
 $curl = new Curl();
+$curl->setProxy('someproxy.com', '9999', 'username', 'password');
 $curl->setOpt(CURLOPT_HTTPPROXYTUNNEL, 1);
-$curl->setOpt(CURLOPT_PROXY, 'someproxy.com:9999');
-$curl->setOpt(CURLOPT_PROXYUSERPWD, 'username:password');
 $curl->get('https://httpbin.org/get');
 var_dump($curl->response);
