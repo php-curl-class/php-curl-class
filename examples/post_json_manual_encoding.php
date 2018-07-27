6ce706da91866b109d0cc270020f4234f72cec14
Zach Borboa <zachborboa@gmail.com>
2018-07-27 01:03:34 -0700

    Fix #532: Add example that uses manual json encoding

---
+++ examples/post_json_manual_encoding.php
@@ -0,0 +1,20 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use \Curl\Curl;
+
+// curl \
+//     -X POST \
+//     -d "{"id":"1","content":"Hello world!","date":"2015-06-30 19:42:21"}" \
+//     "https://httpbin.org/post"
+
+$data = json_encode(array(
+    'id' => '1',
+    'content' => 'Hello world!',
+    'date' => date('Y-m-d H:i:s'),
+), JSON_UNESCAPED_UNICODE);
+
+$curl = new Curl();
+$curl->setHeader('Content-Type', 'application/json');
+$curl->post('https://httpbin.org/post', $data);
+var_dump($curl->response->json);
