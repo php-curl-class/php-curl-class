75ee4ec75e6925d666c6c451e4fa94236a3902bc
Zach Borboa <zachborboa@gmail.com>
2019-04-19 21:57:53 -0700

    Run tests with most recent version first

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,9 +1,9 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${SCRIPT_DIR}"
 
-for d in "dockerfiles/"* ; do
-    echo "Running ${d}" &&
-      pushd "${d}" &&
+find . -type d -mindepth 2 -path "*/dockerfiles/*" | sort --reverse | while read directory; do
+    echo "Running ${directory}" &&
+      pushd "${directory}" &&
       bash "1_build.sh" &&
       bash "2_start.sh" &&
       bash "3_test.sh" &&
