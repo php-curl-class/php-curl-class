12661cffe241738c71606ec037f0c29781c6471e
Zach Borboa <zachborboa@gmail.com>
2019-04-30 00:52:34 -0700

    Fix #581: Add example to retry request based on HTTP status code.

---
+++ examples/get_with_callable_retry_based_on_http_status_code.php
@@ -0,0 +1,22 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use \Curl\Curl;
+
+$max_retries = 3;
+
+$curl = new Curl();
+$curl->setRetry(function ($instance) use ($max_retries) {
+    // Retry when the result of curl_getinfo($instance->curl, CURLINFO_HTTP_CODE) is 500, 503.
+    return $instance->retries < $max_retries && in_array($instance->httpStatusCode, array(500, 503));
+});
+$curl->get('https://httpbin.org/status/503');
+
+if ($curl->error) {
+    echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . "\n";
+    echo 'attempts: ' . $curl->attempts . "\n";
+    echo 'retries: ' . $curl->retries . "\n";
+} else {
+    echo 'Response:' . "\n";
+    var_dump($curl->response);
+}
