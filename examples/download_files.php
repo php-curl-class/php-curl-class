33b748c71adb1ee66e40cd9f11d5fc387a772981
Zach Borboa <zachborboa@gmail.com>
2019-10-31 20:40:52 -0700

    Unify Curl and MultiCurl examples

--- examples/download_files.php
+++ examples/download_files.php
@@ -5,3 +5,4 @@ use \Curl\Curl;
 
 $curl = new Curl();
 $curl->download('https://secure.php.net/images/logos/php-med-trans.png', '/tmp/php-med-trans.png');
+$curl->download('https://upload.wikimedia.org/wikipedia/commons/c/c1/PHP_Logo.png', '/tmp/PHP_Logo.png');
---
+++ examples/download_files_with_callback.php
@@ -0,0 +1,15 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use \Curl\Curl;
+
+$callback = function ($instance, $tmpfile) {
+    $save_to_path = '/tmp/' . basename($instance->url);
+    $fh = fopen($save_to_path, 'wb');
+    stream_copy_to_stream($tmpfile, $fh);
+    fclose($fh);
+};
+
+$curl = new Curl();
+$curl->download('https://secure.php.net/images/logos/php-med-trans.png', $callback);
+$curl->download('https://upload.wikimedia.org/wikipedia/commons/c/c1/PHP_Logo.png', $callback);
