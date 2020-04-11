2c6d228f5871a7a390d6723177fea4fa6ef6e104
Zach Borboa <zachborboa@gmail.com>
2020-04-11 12:19:12 -0700

    Use vendor phpunit when available

--- tests/run.sh
+++ tests/run.sh
@@ -6,10 +6,20 @@ set -x
 # Let test server know we should allow testing.
 export PHP_CURL_CLASS_TEST_MODE_ENABLED="yes"
 
+# Start test server.
 php -S 127.0.0.1:8000 -t PHPCurlClass/ &> /dev/null &
 pid="${!}"
+
+# Determine which phpunit to use.
+if [[ -f "../vendor/phpunit/phpunit/phpunit" ]]; then
+    phpunit_to_use="../vendor/phpunit/phpunit/phpunit"
+else
+    phpunit_to_use="phpunit"
+fi
+
+# Run tests.
 extra_args="${@}"
-phpunit \
+"${phpunit_to_use}" \
     --configuration "phpunit.xml" \
     --debug \
     --verbose \
