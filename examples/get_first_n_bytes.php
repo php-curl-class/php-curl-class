1f62ac4428c6c4ebbf52e157157b4d61f7c97139
Zach Borboa <zachborboa@gmail.com>
2020-04-20 23:03:27 -0700

    Fix #606: Add example for fetching range using Curl::setRange()

---
+++ examples/get_first_n_bytes.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Curl\Curl;
+
+// Fetch first 50 bytes. Server needs to support the Range header.
+$curl = new Curl();
+$curl->setRange('0-49');
+$curl->get('https://code.jquery.com/jquery-1.11.2.min.js');
+
+if ($curl->error) {
+    echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . "\n";
+} else {
+    var_dump($curl->responseHeaders['status-line']); // HTTP/1.1 206 Partial Content
+    var_dump($curl->responseHeaders['content-length']); // 50
+    var_dump($curl->responseHeaders['content-range']); // bytes 0-49/95931
+    var_dump($curl->response);
+}
