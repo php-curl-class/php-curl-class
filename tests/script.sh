e841e7c15ede672b7051ef72eb6fe51ef927fcd2
Zach Borboa <zachborboa@gmail.com>
2021-03-14 16:35:54 -0400

    Use phpunit from vendor directory when available
    Error message:
        tests/script.sh: line 17: phpunit: command not found

--- tests/script.sh
+++ tests/script.sh
@@ -13,9 +13,16 @@ source "check_syntax.sh"
 # Let test server know we should allow testing.
 export PHP_CURL_CLASS_TEST_MODE_ENABLED="yes"
 
+# Determine which phpunit to use.
+if [[ -f "vendor/bin/phpunit" ]]; then
+    phpunit_to_use="vendor/bin/phpunit"
+else
+    phpunit_to_use="phpunit"
+fi
+
 # Run tests.
-phpunit --version
-phpunit --configuration "phpunit.xml" --debug --verbose
+"${phpunit_to_use}" --version
+"${phpunit_to_use}" --configuration "phpunit.xml" --debug --verbose
 if [[ "${?}" -ne 0 ]]; then
     echo "Error: phpunit command failed"
     ((errors++))
