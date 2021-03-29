a315b541bef5d5965f96b112d61dfed363aad472
Zach Borboa <zachborboa@gmail.com>
2021-03-29 00:14:52 -0400

    Consolidate continuous integration scripts
    Merge before_script.sh into script.sh

--- tests/script.sh
+++ tests/script.sh
@@ -1,3 +1,25 @@
+set -x
+
+echo "CI_PHP_VERSION: ${CI_PHP_VERSION}"
+php -r "var_dump(phpversion());"
+php -r "var_dump(curl_version());"
+
+composer self-update
+composer install --prefer-source --no-interaction
+
+# Let test server know we should allow testing.
+export PHP_CURL_CLASS_TEST_MODE_ENABLED="yes"
+
+# Start test servers. Run servers on different ports to allow simultaneous
+# requests without blocking.
+server_count=7
+for i in $(seq 0 $(("${server_count}" - 1))); do
+    port=8000
+    (( port += $i ))
+
+    php -S "127.0.0.1:${port}" -t tests/PHPCurlClass/ &
+done
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${SCRIPT_DIR}"
 
@@ -33,9 +55,6 @@ phpunit_v8_1_shim() {
 errors=()
 
 source "check_syntax.sh"
-
-# Let test server know we should allow testing.
-export PHP_CURL_CLASS_TEST_MODE_ENABLED="yes"
 
 # Determine which phpunit to use.
 if [[ -f "../vendor/bin/phpunit" ]]; then
