ef037f058de1ee5913029bd43d652466a3513a50
Zach Borboa <zachborboa@gmail.com>
2021-06-10 12:17:21 -0400

    Match number of servers started

--- tests/before_script.sh
+++ tests/before_script.sh
@@ -31,14 +31,19 @@ reload_nginx() {
 }
 
 start_php_servers() {
-    for i in $(seq 0 6); do
+    # Start test servers. Run servers on different ports to allow simultaneous
+    # requests without blocking.
+    server_count=7
+    for i in $(seq 0 $(("${server_count}" - 1))); do
         port=8000
         (( port += $i ))
+
         php -S "127.0.0.1:${port}" -t tests/PHPCurlClass/ &
     done
 }
 
 set -x
+
 echo "CI_PHP_VERSION: ${CI_PHP_VERSION}"
 php -r "var_dump(phpversion());"
 php -r "var_dump(curl_version());"
