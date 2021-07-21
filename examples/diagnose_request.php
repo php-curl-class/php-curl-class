a9592df07afbc98f3281ddef16423300e0be3f72
Zach Borboa <zachborboa@gmail.com>
2021-07-20 22:32:51 -0400

    Add example showing Curl::diagnose() usage

---
+++ examples/diagnose_request.php
@@ -0,0 +1,17 @@
+<?php
+// keywords: diagnose, troubleshoot, help
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Curl\Curl;
+
+$curl = new Curl();
+$curl->get('https://httpbin.org/status/400');
+
+if ($curl->error) {
+    echo 'An error occurred:' . "\n";
+    $curl->diagnose();
+} else {
+    echo 'Response:' . "\n";
+    var_dump($curl->response);
+}
