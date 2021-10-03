2c73d6321eaf864cde379ac081bb92e5e3369d93
Zach Borboa <zachborboa@gmail.com>
2021-10-03 18:32:45 -0400

    Fix #684: Add GET request with port specified

---
+++ examples/get_with_port.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Curl\Curl;
+
+// curl --request GET "https://httpbin.org:443/get?key=value"
+
+$curl = new Curl();
+$curl->get('https://httpbin.org:443/get', [
+    'key' => 'value',
+]);
+
+if ($curl->error) {
+    echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . "\n";
+} else {
+    echo 'Response:' . "\n";
+    var_dump($curl->response);
+}
