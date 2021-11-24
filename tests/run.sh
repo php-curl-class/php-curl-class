31a65e2dfbae267e566bb6ebafbf790b946641b0
Zach Borboa <zachborboa@gmail.com>
2021-11-23 21:53:32 -0500

    Remove void return type when running tests on PHP 7.0
    The void return type was implemented in PHP 7.1 and PHP 7.0 treats the
    void return type as a class name.
    
    Errors:
        1) CurlTest\CurlTest::testExtensionsLoaded
        TypeError: Return value of CurlTest\CurlTest::setUp() must be an instance of CurlTest\void, none returned
    
        /home/runner/work/php-curl-class/php-curl-class/tests/PHPCurlClass/PHPCurlClassTest.php:18
    
        2) CurlTest\CurlTest::testCaseInsensitiveArrayGet
        TypeError: Return value of CurlTest\CurlTest::setUp() must be an instance of CurlTest\void, none returned
    
        /home/runner/work/php-curl-class/php-curl-class/tests/PHPCurlClass/PHPCurlClassTest.php:18
    
        3) CurlTest\CurlTest::testCaseInsensitiveArraySet
        TypeError: Return value of CurlTest\CurlTest::setUp() must be an instance of CurlTest\void, none returned
    
        /home/runner/work/php-curl-class/php-curl-class/tests/PHPCurlClass/PHPCurlClassTest.php:18
    
        [...]

--- tests/run.sh
+++ tests/run.sh
@@ -24,6 +24,14 @@ phpunit_v8_1_shim() {
     remove_expectWarning
 }
 
+php_v7_0_shim() {
+    # -protected function setUp(): void
+    # +protected function setUp()
+    find='protected function setUp(): void'
+    replace='protected function setUp()'
+    sed -i'' -e"s/${find}/${replace}/" "./PHPCurlClass/PHP"*
+}
+
 set -x
 
 # Use composer's phpunit and phpcs by adding composer bin directory to the path environment variable.
@@ -71,6 +79,10 @@ elif [[ "${phpunit_version}" == "7.5."* ]]; then
     phpunit_v7_5_shim
 elif [[ "${phpunit_version}" == "8.1."* ]]; then
     phpunit_v8_1_shim
+fi
+
+if [[ "${CI_PHP_VERSION}" == "7.0" ]]; then
+    php_v7_0_shim
 fi
 
 # Run tests.
