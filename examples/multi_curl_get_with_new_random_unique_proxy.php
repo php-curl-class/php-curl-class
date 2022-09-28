cab99aaee5b2a220e0f2aff0cce4ae57ca321383
Zach Borboa <zachborboa@gmail.com>
2022-09-28 14:22:30 -0700

    Add MultiCurl example for retrying with a new unique proxy (#729)

--- examples/multi_curl_get_with_new_random_unique_proxy.php
+++ examples/multi_curl_get_with_new_random_unique_proxy.php
@@ -9,22 +9,30 @@ $proxies = [
     'someproxy.com:80',
     'someproxy.com:443',
 ];
-$max_retries = 3;
+$current_proxies = $proxies;
 
 $multi_curl = new MultiCurl();
 $multi_curl->setProxyType(CURLPROXY_SOCKS5);
-$multi_curl->setProxies($proxies);
-
-$multi_curl->setRetry(function ($instance) use ($proxies, $max_retries) {
-    if ($instance->retries < $max_retries) {
-        $new_random_proxy = ArrayUtil::arrayRandom($proxies);
-        $instance->setProxy($new_random_proxy);
-        return true;
-    } else {
-        return false;
+$multi_curl->beforeSend(function ($instance) use ($proxies, &$current_proxies) {
+    if (!count($current_proxies)) {
+        $current_proxies = $proxies;
     }
+
+    // Use random proxy that hasn't been used yet.
+    $rand_key = ArrayUtil::arrayRandomIndex($current_proxies);
+    $new_random_proxy = $current_proxies[$rand_key];
+    $instance->setProxy($new_random_proxy);
+
+    // Remove proxy from list of current proxies.
+    unset($current_proxies[$rand_key]);
+
+    // Re-index list of current proxies.
+    $current_proxies = array_values($current_proxies);
+
+    echo 'about to make request ' . $instance->id . ' using proxy "' . $new_random_proxy . '".' . "\n";
 });
 
+$multi_curl->addGet('https://httpbin.org/ip');
 $multi_curl->addGet('https://httpbin.org/ip');
 $multi_curl->addGet('https://httpbin.org/ip');
 $multi_curl->addGet('https://httpbin.org/ip');
