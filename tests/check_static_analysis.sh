5b388dd4dd3e69f9fb65fbf40cce0d5f330c726c
Zach Borboa <zachborboa@gmail.com>
2022-12-16 05:14:40 -0800

    Add script to run static analysis checks

---
+++ tests/check_static_analysis.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${SCRIPT_DIR}"
+
+# Run commands from the project root directory.
+cd ..
+
+set -x
+
+vendor/bin/psalm --config="tests/psalm.xml"
