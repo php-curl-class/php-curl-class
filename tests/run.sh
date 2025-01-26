fa9f97a7d2f472d4df06177b718db8139b8a5537
Zach Borboa <zachborboa@gmail.com>
2025-01-26 11:33:15 -0800

    Remove temporary workaround

--- tests/run.sh
+++ tests/run.sh
@@ -4,19 +4,6 @@ set -x
 
 if [[ "${CI}" == "true" ]]; then
     composer self-update
-
-    # TODO: Add "vimeo/psalm" back into composer.json under "require-dev" when
-    # vimeo/psalm supports PHP 8.4 (https://github.com/vimeo/psalm/issues/11107):
-    #     "require-dev": {
-    #         "vimeo/psalm": ">=5.26.1"
-    #     },
-    #
-    # TODO: Remove this workaround that only installs vimeo/psalm on PHP < 8.4 when
-    # vimeo/psalm supports PHP 8.4 (https://github.com/vimeo/psalm/issues/11107):
-    if [[ $(echo "${CI_PHP_VERSION} < 8.4" | bc -l) -eq 1 ]]; then
-        composer require --dev "vimeo/psalm:>=5.26.1"
-    fi
-
     composer install --prefer-source --no-interaction
 fi
 
